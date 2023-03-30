b71a6683b300c55082d0a2ea8e98d0f37b91374f
merveBILGIC <merveblgc82@gmail.com>
2023-03-30 16:54:01 +0300

    add mvc

---
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.Services;
+
+namespace NLayer.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+       
+        public ProductsController(IProductService productService = null)
+        {
+            _productService = productService;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var CustomResponse = await _productService.GetProductsWithCategory();
+            return View( CustomResponse.Data);
+        }
+    }
+}
